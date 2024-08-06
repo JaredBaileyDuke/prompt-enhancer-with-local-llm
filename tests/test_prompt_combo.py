332f1852024059932ff1b2a782d8a3e70c09ba41
JaredBaileyDuke <jared.l.bailey@duke.edu>
2024-08-06 14:41:25 -0400

    updated fixes for linting

--- tests/test_prompt_combo.py
+++ tests/test_prompt_combo.py
@@ -26,51 +26,42 @@ def test_prompt_combo():
     Test the prompt_combo function
     """
     # Test case 1
-    assert prompt_combo("Where you are from", "New York", "New York City", "NYC") == (
-        "Where you are from. Your response should have the following attributes: "
-        "New York. New York City. NYC"
-        )
+    assert prompt_combo("Where you are from", "New York", "New York City", "NYC") == \
+        "Where you are from. Your response should have the following attributes: \
+        New York. New York City. NYC"
     # Test case 2
-    assert prompt_combo("What you are doing", "Working", "Studying", "Playing") == (
-        "What you are doing. Your response should have the following attributes: "
-        "Working. Studying. Playing"
-        )
+    assert prompt_combo("What you are doing", "Working", "Studying", "Playing") == \
+        "What you are doing. Your response should have the following attributes: \
+        Working. Studying. Playing"
     # Test case 3
     assert prompt_combo("What you are eating", "Pizza", "Burger", "Salad") == \
-        "What you are eating. Your response should have the following attributes: Pizza. Burger. \
-            Salad"
-    
+        "What you are eating. Your response should have the following attributes: \
+        Pizza. Burger. Salad"    
     # Test case 4
     assert prompt_combo("What you are drinking", "Water", "Soda", "Juice") == \
-        "What you are drinking. Your response should have the following attributes: Water. Soda. \
-            Juice"
-    
+        "What you are drinking. Your response should have the following attributes: \
+        Water. Soda. Juice"    
     # Test case 5
     assert prompt_combo("What you are wearing", "Shirt", "Pants", "Shoes") == \
-        "What you are wearing. Your response should have the following attributes: Shirt. Pants. \
-            Shoes"
-    
+        "What you are wearing. Your response should have the following attributes: \
+        Shirt. Pants. Shoes" 
     # Test case 6
     assert prompt_combo("What you are watching", "Movie", "TV Show", "Documentary") == \
-        "What you are watching. Your response should have the following attributes: Movie. \
-            TV Show. Documentary"
-    
+        "What you are watching. Your response should have the following attributes: \
+            Movie. TV Show. Documentary"
     # Test case 7
     assert prompt_combo("What you are reading", "Book", "Magazine", "Newspaper") == \
-        "What you are reading. Your response should have the following attributes: Book. \
-            Magazine. Newspaper"
-    
+        "What you are reading. Your response should have the following attributes: \
+            Book. Magazine. Newspaper"
     # Test case 8
     assert prompt_combo("What you are listening to", "Music", "Podcast", "Audiobook") == \
-        "What you are listening to. Your response should have the following attributes: Music. \
-            Podcast. Audiobook"
-    
+        "What you are listening to. Your response should have the following attributes: \
+            Music. Podcast. Audiobook"
     # Test case 9
     assert prompt_combo("What you are playing", "Game", "Sport", "Instrument") == \
-        "What you are playing. Your response should have the following attributes: Game. Sport. \
-            Instrument"
-    
+        "What you are playing. Your response should have the following attributes: \
+            Game. Sport. Instrument"
     # Test case 10
     assert prompt_combo("What you are learning", "Language", "Skill", "Subject") == \
-        "What you are learning. Your response should have the following attributes: Language. \
-            Skill. Subject"
+        "What you are learning. Your response should have the following attributes: \
+            Language. Skill. Subject"
